cc7f5d2dbcc08ef8cd96013a42219906c001311e
Poletansky Viktor <poletansky@gmail.com>
2022-12-20 00:43:08 +0300

    simplify export

--- src/routes/Vec2.ts
+++ src/routes/Vec2.ts
@@ -1,27 +1,29 @@
-type Vec2 = {
+export type Vec2 = {
   x: number,
   y: number,
 };
 
-function minus(v: Vec2, w: Vec2): Vec2 {
+export function minus(v: Vec2, w: Vec2): Vec2 {
   return {x: v.x - w.x, y: v.y - w.y}
 }
 
-function plus(v: Vec2, w: Vec2): Vec2 {
+export function plus(v: Vec2, w: Vec2): Vec2 {
   return {x: v.x + w.x, y: v.y + w.y}
 }
 
-function multiply(v: Vec2, c: number): Vec2 {
+export function multiply(v: Vec2, c: number): Vec2 {
   return {x: v.x * c, y: v.y * c};
 }
 
-function cross(v: Vec2, w: Vec2): number {
+export function cross(v: Vec2, w: Vec2): number {
   return v.x * w.y - v.y * w.x;
 }
 
-function dot(v: Vec2, w: Vec2): number {
+export function dot(v: Vec2, w: Vec2): number {
   return v.x * w.x + v.y * w.y;
 }
 
-export {Vec2, minus, plus, multiply, cross, dot};
+export function distSquared(v: Vec2): number {
+  return dot(v, v);
+}
 
